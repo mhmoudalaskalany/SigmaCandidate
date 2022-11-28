4d1ec070d0b538bcb3840eb3fb74bb9e0b4d2cd0
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2022-11-28 11:42:13 +0400

    check for duplicate email at add

--- Candidate.Application/Services/Candidate/CandidateService.cs
+++ Candidate.Application/Services/Candidate/CandidateService.cs
@@ -35,6 +35,11 @@ namespace Candidate.Application.Services.Candidate
 
         public async Task<string> AddAsync(AddCandidateDto model)
         {
+            var isExist = await _candidateRepository(_infrastructureType.ToString()).Any(model.Email);
+            if (isExist)
+            {
+                throw new BusinessException("Email Already Exist" , null);
+            }
             var entity = _mapper.Map<Domain.Entities.Candidate>(model);
             await _candidateRepository(_infrastructureType.ToString()).AddAsync(entity);
             return entity.Email;
